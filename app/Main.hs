8feda1e02209ae7ea847892ccd3d99b6759e16a4
Gennadi Heimann <genaheimann@gmail.com>
2021-07-23 11:38:58 +0200

    weitere Versuche

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,11 @@ module Main where
 
 import Lib
 import WServer
-import TestJson
+import TestJson ( printReq, printRes, getClientId )
 
 main :: IO ()
 main = do 
     print printReq
     print printRes
+    print  getClientId
 -- main = WServer.runServer
--- src/TestJson.hs
+++ src/TestJson.hs
@@ -1,8 +1,9 @@
-module TestJson(printReq, printRes) where
+module TestJson(printReq, printRes, getClientId) where
 
 import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BS
-import GHC.Generics
+import GHC.Generics ()
+import Control.Monad ( MonadPlus(mzero) )
 
 {-
 JSON-RPC
@@ -16,9 +17,8 @@ CalcExactRoot
 
 
 
-data Req = Req { 
+data Req = Req {
     reqId :: String
-  , jsonrpc :: String
   ,  method :: String
   ,  params :: Maybe Value
 } deriving (Show, Eq)
@@ -26,23 +26,45 @@ data Req = Req {
 instance FromJSON Req where
   parseJSON (Object v) = Req <$>
                         v .: "id" <*>
-                        v .: "jsonrpc" <*>
                         v .: "method" <*>
                         v .: "params"
+  parseJSON _          = mzero
 
-data Resp = OkResp String String deriving Show
+data Resp = OkResp String Value deriving Show
 
 reqPRC :: BS.ByteString
-reqPRC = "{\"jsonrpc\": \"2.0\", \"method\": \"register\", \"params\": {\"clientId\": 123452}, \"id\": \"1\"}"
+reqPRC = "{\"jsonrpc\": \"2.0\", \"method\": \"register\", \"params\": {\"clientId\": 123452, \"clientName\" : \"test\"}, \"id\": \"1\"}"
 
 instance ToJSON Resp where
-  toJSON (OkResp rid result) = object [ "jsonrpc" .= rid , "id" .= rid, "result" .= result ]
+  toJSON (OkResp rid result) = object ["id" .= rid, "result" .= result]
 
 printReq :: Either String Req
-printReq = eitherDecode reqPRC 
-
-resRPC :: Resp
-resRPC = OkResp "1" "test"
+printReq = eitherDecode reqPRC
 
 printRes :: BS.ByteString
 printRes = encode $ OkResp "1" "test"
+
+getMaybeReq :: Maybe Req
+getMaybeReq = decode reqPRC
+  
+getClientId :: Value
+getClientId = case getMaybeReq of
+    Just r -> case params r of
+        Just p ->  p
+        Nothing -> "error"
+    Nothing -> "error" 
+
+extractClientId :: Maybe Value
+extractClientId =  params =<< getMaybeReq
+
+-- extractClientId2 :: String
+-- extractClientId2 = parseMaybe extractClientId
+    
+    
+{-    case getMaybeReq of
+    Just r  -> parseMaybe (decode (params r)) $ \o -> do 
+        clientId <- o .: "clientId"
+        name <- o .: "clientName"
+        return (show clientId)
+    Nothing -> "" 
+    -}
